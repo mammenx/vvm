b315f587c8a01e755343bbe042b334c485c29adc
Gregory James <mammenx@gmail.com>
2019-12-14 15:48:12 +0530

    Changed vvm_clk_info_t from struct to its own class & enhanced the clock-generator to take init_val & start_delay inputs.

--- methodology/vvm_clk_gen.h
+++ methodology/vvm_clk_gen.h
@@ -2,27 +2,37 @@
 #define __VVM_CLK_GEN
 
 #include <vector>
+#include <string>
 #include "base/vvm_types.h"
 #include "base/vvm_component.h"
+#include "base/vvm_object.h"
 
-typedef struct  vvm_clk_info_t  {
-  unsigned  char* dut_clk_ref;
-  unsigned  int   phase0_steps;
-  unsigned  int   phase1_steps;
-  unsigned  int   next_tickcount;
-} vvm_clk_info_t;
+class vvm_clk : vvm_object  {
+  public:
+    std::string     name;
+    unsigned  char* dut_clk_ref;
+    unsigned  int   phase0_steps;
+    unsigned  int   phase1_steps;
+    unsigned  int   init_val;
+    unsigned  int   start_delay;
+    unsigned  int   next_tickcount;
+
+    void  print_clk_state() {
+      printf("[Clock %s] curr_val : %d, phase0_steps : %d, phase1_steps : %d, start_delay : %d, init_val : %d, next_tickcount : %d\n",name.c_str(),*(dut_clk_ref),phase0_steps,phase1_steps,start_delay,init_val,next_tickcount);
+    }
+};
 
 template<class DUT_T>  class vvm_clk_gen : vvm_component {
   private:
     DUT_T*  dut_ref;
-    std::vector<vvm_clk_info_t> clk_info_vec;
+    std::vector<vvm_clk> clk_info_vec;
     unsigned  long  m_tickcount;
 
   public:
     vvm_clk_gen(DUT_T* dut):dut_ref(dut) {};
-    vvm_status_t                add_dut_clock(unsigned  char* dut_clk_ref,  unsigned  int period);
+    vvm_status_t                add_dut_clock(std::string name,  unsigned  char* dut_clk_ref,  unsigned  int period,  unsigned int init_val=0, unsigned int start_delay=0);
     void                        init_dut_clocks(void);
-    std::vector<vvm_clk_info_t> toggle_next_clock(void);
+    std::vector<vvm_clk>        toggle_next_clock(void);
     unsigned  long              get_tickcount(void);
 
 };
--- methodology/vvm_clk_gen.imp.h
+++ methodology/vvm_clk_gen.imp.h
@@ -1,12 +1,16 @@
 #include  "methodology/vvm_clk_gen.h"
 #include  "base/vvm_component.h"
+#include  <string>
 
-template<class DUT_T>  vvm_status_t  vvm_clk_gen<DUT_T>::add_dut_clock(unsigned  char* dut_clk_ref,  unsigned  int period)  {
-  vvm_clk_info_t  clk_info;
+template<class DUT_T>  vvm_status_t  vvm_clk_gen<DUT_T>::add_dut_clock(std::string name, unsigned  char* dut_clk_ref,  unsigned  int period, unsigned int init_val, unsigned int start_delay)  {
+  vvm_clk clk_info;
 
+  clk_info.name         = name;
   clk_info.dut_clk_ref  = dut_clk_ref;
   clk_info.phase0_steps = period  / 2;
   clk_info.phase1_steps = period  - clk_info.phase0_steps;
+  clk_info.init_val     = init_val;
+  clk_info.start_delay  = start_delay;
 
   clk_info_vec.push_back(clk_info);
 
@@ -17,15 +21,15 @@ template<class DUT_T>  void  vvm_clk_gen<DUT_T>::init_dut_clocks(void) {
   m_tickcount = 0;
 
   for(int i=0;  i<clk_info_vec.size();  i++)  {
-    *(clk_info_vec[i].dut_clk_ref)  = 1;
-    clk_info_vec[i].next_tickcount  = m_tickcount + clk_info_vec[i].phase0_steps;
+    *(clk_info_vec[i].dut_clk_ref)  = clk_info_vec[i].init_val;
+    clk_info_vec[i].next_tickcount  = m_tickcount + clk_info_vec[i].start_delay + clk_info_vec[i].phase0_steps;
   }
 
   dut_ref->eval();
 }
 
-template<class DUT_T>  std::vector<vvm_clk_info_t>  vvm_clk_gen<DUT_T>::toggle_next_clock(void) {
-  std::vector<vvm_clk_info_t>  result;
+template<class DUT_T>  std::vector<vvm_clk>  vvm_clk_gen<DUT_T>::toggle_next_clock(void) {
+  std::vector<vvm_clk>  result;
 
   while(1)  {
     for(int i=0;  i<clk_info_vec.size();  i++)  {
